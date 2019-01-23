25fa691a62c334bc27f85026bb9926e9c6a5630f
jpliu24 <ray0924@gmail.com>
2019-01-23 21:15:20 +0800

    Update moving-entries.py
    add comment

--- moving-entries.sikuli/moving-entries.py
+++ moving-entries.sikuli/moving-entries.py
@@ -1,12 +1,17 @@
 click(Pattern("1548237032927.png").similar(0.60))
 
+
+#still exists entries in temporary account
 while exists(Pattern("1548237318013.png").similar(0.85)):
+
+    #change its account entry by entry
     click(Pattern("1548237318013.png").similar(0.85).targetOffset(157,3))
     click(Pattern("1548237474102.png").similar(0.80))
     click(Pattern("1548237512982.png").similar(0.80))
-    
     wait("1548237952159.png")
     click(Pattern("1548237952159.png").similar(0.75).targetOffset(64,-14))
+
+    #dirty method to reselect a new entry to move
     wait(Pattern("1548237318013.png").similar(0.85))
     click(Pattern("1548238041498.png").similar(0.80).targetOffset(-37,2))
     wait("1548238102442.png")
